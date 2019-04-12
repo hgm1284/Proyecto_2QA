a28342cae432cdade35bb9b9cee0e86ecf9e6da5
hgm1284 <hygomezm@est.utn.ac.cr>
2019-04-12 16:09:41 -0600

    Search

---
+++ TestManager/src/test/java/Test/SearchNullTest.java
@@ -0,0 +1,22 @@
+package Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import Pages.SearchPage;
+
+public class SearchNullTest extends BaseTest {
+	
+	@Before
+	public void open(){
+		driver.get("https://elaulaenlinea.com/course/index.php");
+	}
+	
+	@Test
+	public void SearchCredenciales() {
+	SearchPage searchPage = new SearchPage(driver);
+	searchPage.enterCredentials("");
+	searchPage.clickLoginButton();			
+	}
+	
+
+}
---
+++ TestManager/src/test/java/Test/SearchTest.java
@@ -0,0 +1,22 @@
+package Test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import Pages.SearchPage;
+
+public class SearchTest extends BaseTest {
+	
+	@Before
+	public void open(){
+		driver.get("https://elaulaenlinea.com/course/index.php");
+	}
+	
+	@Test
+	public void SearchCredenciales() {
+	SearchPage searchPage = new SearchPage(driver);
+	searchPage.enterCredentials("Auditoría de Tecnologías de Información");
+	searchPage.clickLoginButton();			
+	}
+
+}
